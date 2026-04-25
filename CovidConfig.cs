115c2c7043a59a7158bdc6c34d46dbcba170c8f9
dynastygunawan22 <dynastygunawan22@gmail.com>
2026-04-25 20:43:47 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpmodul8_103082400037
+{
+    internal class CovidConfig
+    {
+    }
+}
--- obj/Debug/net10.0/tpmodul8_103082400037.AssemblyInfo.cs
+++ obj/Debug/net10.0/tpmodul8_103082400037.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul8_103082400037")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b76ff4fc228c6b7d5b0a20d13c6f0185a717c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul8_103082400037")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul8_103082400037")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
